939d3cdfd8eaf2abf4dcccf0fdc79d8500e8b34c
Divyesh Sharma Badoreea <64778752+adarsh2515@users.noreply.github.com>
2020-05-07 18:55:41 +0400

    Add files via upload

---
+++ CRUD_INFO.sql
@@ -0,0 +1,12 @@
+Create Database CRUD_tutorial
+
+Create Table UserInfo_Tab(
+ID int, 
+Name  varchar(50),
+Address  varchar(50),
+City  varchar(50),
+Age  int,
+Sex  varchar(50)
+)
+
+Select * from UserInfo_Tab
